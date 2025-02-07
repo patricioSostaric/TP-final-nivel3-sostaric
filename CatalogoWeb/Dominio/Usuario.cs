d358b905a7d590d0ccecbc9ac879444bd3637d69
Patricio sostaric <patricio.jp.01sostaric@gmail.com>
2025-02-07 11:54:44 -0300

    creacion clase UsuarioNegocio

--- CatalogoWeb/Dominio/Usuario.cs
+++ CatalogoWeb/Dominio/Usuario.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace Dominio
 {
-    internal class Usuarios
+    public class Usuario
     {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Pass { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string UrlImagenPerfil { get; set; }
+        public bool Admin { get; set; }
+
+
+
     }
 }
---
+++ CatalogoWeb/Negocio/UsuarioNegocio.cs
@@ -0,0 +1,95 @@
+﻿using Datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+
+namespace Negocio
+{
+    public class UsuarioNegocio
+    {
+        public bool Login(Usuario usuario)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("Select id, email, pass, nombre, apellido, urlImagenPerfil, admin from USERS where email = @email and pass = @pass");
+                datos.setearParametro("@email", usuario.Email);
+                datos.setearParametro("@pass", usuario.Pass);
+                datos.ejecutarLectura();
+                if (datos.Lector.Read())
+                {
+                    usuario.Id = (int)datos.Lector["id"];
+                    usuario.Admin = (bool)datos.Lector["admin"];
+                    if (!(datos.Lector["nombre"] is DBNull))
+                        usuario.Nombre = (string)datos.Lector["nombre"];
+                    if (!(datos.Lector["apellido"] is DBNull))
+                        usuario.Apellido = (string)datos.Lector["apellido"];
+                    if (!(datos.Lector["urlImagenPerfil"] is DBNull))
+                        usuario.UrlImagenPerfil  = (string)datos.Lector["urlImagenPerfil"];
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+        public int insertarNuevo(Usuario nuevo)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearProcedimiento("insertarNuevo");
+                datos.setearParametro("@email", nuevo.Email);
+                datos.setearParametro("@pass", nuevo.Pass);
+                return datos.ejecutarAccionScalar();
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+            public void actualizar(Usuario user)
+            {
+                AccesoDatos datos = new AccesoDatos();
+                try
+                {
+                    datos.setearConsulta("update USERS set urlImagenPerfil = @imagen, nombre = @nombre, apellido = @apellido where Id = @id");
+                    //datos.setearParametro("@imagen", user.UrlImagenPerfil != null ? user.ImagenPerfil : (object)DBNull.Value);
+                    datos.setearParametro("@imagen", (object)user.UrlImagenPerfil ?? DBNull.Value);
+                    datos.setearParametro("@nombre", user.Nombre);
+                    datos.setearParametro("@apellido", user.Apellido);
+                    datos.setearParametro("id", user.Id);
+                    datos.ejecutarAccion();
+                }
+                catch (Exception ex)
+                {
+
+                    throw ex;
+                }
+                finally
+                {
+                    datos.cerrarConexion();
+                }
+            }
+
+      }
+   
+}
+
